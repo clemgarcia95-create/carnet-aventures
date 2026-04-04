e6a4a015f609e51f27ecdd2fb9e65e28d660779a
clemgarcia95-create <clem.garcia95@gmail.com>
2026-04-05 00:21:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ conn = init_connection()
 
 # Fonction pour récupérer les données
 def get_data():
-    response = conn.query("*", table="carnet_aventures", ttl=0).execute()
+response = conn.table("carnet_aventures").select("*").execute()
     return response.data
 
 data = get_data()
